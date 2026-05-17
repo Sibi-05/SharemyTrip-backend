af9f2e4db71924114fd299fb2a7bf0bf735acff2
sibi <ssibi769@gmail.com>
2026-05-17 21:22:23 +0530

    get profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,6 +44,32 @@ const uploadProfileImage = async (req, res) => {
 
 };
 
+
+const getProfile = async (
+  req,
+  res
+) => {
+  try {
+    const user =
+      await User.findById(
+        req.user.id
+      ).select("-password");
+
+    res.status(200).json(user);
+
+  } catch (error) {
+    res.status(500).json({
+      message:
+        "Failed to fetch profile",
+    });
+  }
+};
+
 module.exports = {
-    uploadProfileImage
+  getProfile,
+};
+
+module.exports = {
+    uploadProfileImage,
+    getProfile
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,24 @@
 const express = require("express");
 const authMiddleware = require("../middleware/authMiddleware");
 const upload = require("../middleware/upload");
-const {uploadProfileImage} = require("../controllers/userController.js");
 const {
-    registerUser,
-    loginUser
-} = require("../controllers/auth");
+  uploadProfileImage,
+  getProfile,
+} = require("../controllers/userController.js");
+const { registerUser, loginUser } = require("../controllers/auth");
 
 const router = express.Router();
-
 
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
 
 router.put(
-    "/profile-image",
-    authMiddleware,
-    upload.single("profilePic"),
-    uploadProfileImage
+  "/profile-image",
+  authMiddleware,
+  upload.single("profilePic"),
+  uploadProfileImage,
 );
-
-
+router.get("/profile", authMiddleware, getProfile);
 
 module.exports = router;
